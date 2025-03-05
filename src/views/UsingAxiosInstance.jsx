bbb6287209a3c6e0778ab8f72ed321d3341a9c21
Anand6262 <pyexpert62@gmail.com>
2025-03-05 23:46:57 +0530

    changes\

--- src/views/UsingAxiosInstance.jsx
+++ src/views/UsingAxiosInstance.jsx
@@ -1,6 +1,22 @@
-import React from 'react'
+import React, { useEffect} from 'react'
+import { apiGetDummyData } from '../services/DummyApi';
 
 function UsingAxiosInstance() {
+
+  useEffect (() => {
+    fetchDummyData();
+  }, [])
+
+  const fetchDummyData = async () => {
+    try {
+      const response = await apiGetDummyData();
+      // console.log('response :', response);
+        
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <div>UsingAxiosInstance</div>
   )
